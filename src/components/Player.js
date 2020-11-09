706e9edc378316fb8d728dd634bb5a4a1adcec75
LAPTOP-U003GCDR\anguy <nducanh2311@gmail.com>
2020-11-09 23:10:36 +0700

    Create initial components and styling

---
+++ src/components/Player.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+export default function Player() {
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range" />
+        <p>End Time</p>
+      </div>
+      <div className="play-control">
+        <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
+        <FontAwesomeIcon className="play" icon={faPlay} size="2x" />
+        <FontAwesomeIcon className="skip-forw" icon={faAngleRight} size="2x" />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Song() {
+  return (
+    <div>
+      <div className="song-container">
+        <h2>Pictures</h2>
+        <h2>Song Name</h2>
+        <h2>Artist</h2>
+      </div>
+    </div>
+  );
+}
